16b9c134a859ee0333102bb0bf896315ab94d442
Felicity Semetey <felicitysemetey628@gmail.com>
2024-10-07 16:02:02 +0000

    new

---
+++ models/book.js
@@ -0,0 +1,2 @@
+// use "types" for models that have relationships. so in the case of reviews and authors, many reviews map to one book and many books map to one author. Hence, the relationship.
+import { Schema, model} from "mongoose";
